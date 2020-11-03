1888b76783b6be1834012f0a47f272d51422fa1a
Alex Shaposhnikov <alexxforcode@gmail.com>
2020-11-03 18:04:27 +0300

    login-page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAuHH5NCa2tRbcUGrSLOYXSeSk0Imuki70'
 };
